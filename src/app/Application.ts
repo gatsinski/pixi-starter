52d925b3adfdbeafc5bc495f1730bb43a36cfa27
Hristo Gatsinski <gatsinski@gmail.com>
2021-08-15 12:53:15 +0300

    Add center getter to Application

--- src/app/Application.ts
+++ src/app/Application.ts
@@ -3,6 +3,13 @@ import { Service } from "typedi";
 
 @Service()
 export default class Application extends PIXI.Application {
+    public get center(): PIXI.Point {
+        const x = this.renderer.width / 2;
+        const y = this.renderer.height / 2;
+
+        return new PIXI.Point(x, y);
+    }
+
     constructor() {
         super({ width: 1280, height: 768 });
 
