27c214c6a2f0719fcd0b7cdac7b068f9b0b29e72
Hristo Gatsinski <gatsinski@gmail.com>
2021-09-28 23:21:28 +0300

    Add environments

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,14 @@
 const { merge } = require("webpack-merge");
+const webpack = require("webpack");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(
+      /environments\/environment\.ts/,
+      "./environment.prod.ts"
+    ),
+  ],
 });
